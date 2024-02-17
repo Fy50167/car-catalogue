f588c041ba1d919eb4f0ccfe601f7232626b1ff6
Francis Yang <fy50167@gmail.com>
2024-02-16 18:10:14 -0800

    update fetch function

--- utils/index.ts
+++ utils/index.ts
@@ -1,16 +1,14 @@
-const url = 'https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?model=corolla';
-const options = {
-	method: 'GET',
-	headers: {
+export async function fetchCars() {
+   const headers = {
 		'X-RapidAPI-Key': 'f2f309773emsh6f6eb1c241fae98p1ff76djsn1602c81f0ba2',
 		'X-RapidAPI-Host': 'cars-by-api-ninjas.p.rapidapi.com'
-	}
-};
+   } 
 
-try {
-	const response = await fetch(url, options);
-	const result = await response.text();
-	console.log(result);
-} catch (error) {
-	console.error(error);
+   const response = await fetch('https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?model=corolla', {
+    headers: headers,
+   });
+
+   const result = await response.json();
+
+   return result
 }
