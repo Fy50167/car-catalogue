a632ec74169ca30882897e9367065c58f1bdfa8b
Francis Yang <fy50167@gmail.com>
2024-02-16 19:23:10 -0800

    display demo car in car card

--- components/CarCard.tsx
+++ components/CarCard.tsx
@@ -4,6 +4,7 @@ import { useState } from 'react';
 import Image from 'next/image';
 import { CarProps } from '@/types';
 import CustomButton from './CustomButton';
+import { calculateCarRent } from '@/utils';
 
 interface CarCardProps {
     car: CarProps;
@@ -12,5 +13,37 @@ interface CarCardProps {
 export default function CarCard({ car }: CarCardProps) {
     const { city_mpg, year, make, model, transmission, drive } = car;
 
-    return <div className='car-card group'></div>;
+    const carRent = calculateCarRent(city_mpg, year);
+
+    return (
+        <div className='car-card group'>
+            <div className='car-card__content'>
+                <h2 className='car-card__content-title'>
+                    {make} {model}
+                </h2>
+            </div>
+
+            <p className='flex mt-6 text-[32px] font-extrabold'>
+                <span className='self self-start text-[14px] font-semibold'>
+                    $
+                </span>
+                {carRent}
+                <span className='self self-end text-[14px] font-medium'>
+                    /day
+                </span>
+            </p>
+
+            <div className='relative w-full h-4 my-3 object-contain'>
+                <Image
+                    src='/hero.png'
+                    width={50}
+                    height={50}
+                    alt='car model'
+                    fill
+                    priority
+                    className='object-contain'
+                />
+            </div>
+        </div>
+    );
 }
