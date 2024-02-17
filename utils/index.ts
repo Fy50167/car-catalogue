1231fb70e8fa9aea0a9c9df7e36dc5818a335016
Francis Yang <fy50167@gmail.com>
2024-02-16 18:04:40 -0800

    import rapid api info

---
+++ utils/index.ts
@@ -0,0 +1,16 @@
+const url = 'https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?model=corolla';
+const options = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': 'f2f309773emsh6f6eb1c241fae98p1ff76djsn1602c81f0ba2',
+		'X-RapidAPI-Host': 'cars-by-api-ninjas.p.rapidapi.com'
+	}
+};
+
+try {
+	const response = await fetch(url, options);
+	const result = await response.text();
+	console.log(result);
+} catch (error) {
+	console.error(error);
+}
