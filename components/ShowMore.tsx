e51b35459c37249e9f42fa4e416011a2de29c18b
Francis Yang <fy50167@gmail.com>
2024-02-20 09:28:05 -0800

    show more functionality finished

--- components/ShowMore.tsx
+++ components/ShowMore.tsx
@@ -3,10 +3,16 @@
 import { useRouter } from 'next/navigation';
 import { ShowMoreProps } from '@/types';
 import CustomButton from './CustomButton';
+import { updateSearchParams } from '@/utils';
 
 export default function ShowMore({ pageNumber, isNext }: ShowMoreProps) {
     const router = useRouter();
-    const handleNavigation = () => {};
+    const handleNavigation = () => {
+        const newLimit = (pageNumber + 1) * 10;
+        const newPathName = updateSearchParams('limit', `${newLimit}`);
+
+        router.push(newPathName, { scroll: false });
+    };
 
     return (
         <div className='w-full flex-center gap-5 mt-10'>
